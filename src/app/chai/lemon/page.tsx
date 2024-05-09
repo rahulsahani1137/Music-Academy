8c2e74faa6b429454498271e4d5808ab16f14b7e
Rahul Sahani <yourahul1917@gmail.com>
2024-05-09 15:38:18 +0530

    Created Navbar component

---
+++ src/app/chai/lemon/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function lemonTea() {
+  return (
+    <div className='text-9xl flex justify-center items-center h-[80vh]'>lemonTea</div>
+  )
+}
+
+export default lemonTea
---
+++ src/app/chai/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function chaiPage() {
+  return (
+    <h1 className='text-9xl items-center h-screen flex justify-center'>chai page</h1>
+  )
+}
